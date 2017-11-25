ad7e9af301cc6fdf60a0db7da89a7f7f183a941b
肖俊 <xxzure@xiaojundeMacBook-Pro.local>
2017-11-25 21:24:24 +0800

    change

--- lab3/urls.py
+++ lab3/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'lab3.views.home', name='home'),
